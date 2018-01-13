ea23ba676f504d44411e0e4c8bcc3c0d469d6719
0xxd0 <10173746+0xxd0@users.noreply.github.com>
2018-01-14 00:27:07 +0800

    Update @available

--- Sources/Ivar.swift
+++ Sources/Ivar.swift
@@ -10,17 +10,20 @@ import ObjectiveC.runtime
 final public class Ivar: Inspectable<ObjectiveC.Ivar> {
     
     /// Returns the name of an instance variable.
+    @available(iOS 2.0, macOS 10.5, tvOS 9.0, watchOS 2.0, *)
     public lazy var name: String? = {
         let name = ivar_getName(self.value)
         return name != nil ? String(cString: name!) : nil
     }()
     
     /// Returns the offset of an instance variable.
+    @available(iOS 2.0, macOS 10.5, tvOS 9.0, watchOS 2.0, *)
     public lazy var offset: Int = {
         return ivar_getOffset(self.value)
     }()
     
     /// Returns the type string of an instance variable.
+    @available(iOS 2.0, macOS 10.5, tvOS 9.0, watchOS 2.0, *)
     public lazy var typeEncoding: String? = {
         let encode = ivar_getTypeEncoding(self.value)
         return encode != nil ? String(cString: encode!) : nil
