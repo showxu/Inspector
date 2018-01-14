d04b4b3ab4b42b97585eeeebd9b2cb058fe9be72
0xxd0 <10173746+0xxd0@users.noreply.github.com>
2018-01-14 17:19:48 +0800

    Remove perfromance tests

--- Tests/InspectorTests/InspectorTests.swift
+++ Tests/InspectorTests/InspectorTests.swift
@@ -22,9 +22,9 @@ class ClassTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        measure {
-            print(allClass)
-        }
+//        measure {
+//            print(allClass)
+//        }
     }
 
     static var allTests = [
